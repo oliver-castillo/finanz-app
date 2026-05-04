608b4b7fe7481325edc2643856bbe7a28326e8f9
Oliver Castillo <olcastillot@gmail.com>
2026-05-03 22:17:08 -0500

    feat: initialize project structure with Spring Boot and Maven setup

---
+++ src/test/java/com/app/finanz/FinanzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.finanz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
