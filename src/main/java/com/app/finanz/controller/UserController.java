2a031f8d71312844abd5ae30f99e4de92deba43b
Oliver Castillo <olcastillot@gmail.com>
2026-05-03 23:17:49 -0500

    feat: add UserController for handling user operations

---
+++ src/main/java/com/app/finanz/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.app.finanz.controller;
+
+import com.app.finanz.domain.model.User;
+import com.app.finanz.domain.service.UserService;
+import com.app.finanz.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.openapitools.api.UsersApi;
+import org.openapitools.model.CreateUserRequest;
+import org.openapitools.model.UpdateUserRequest;
+import org.openapitools.model.UserResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for handling user operations.
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController implements UsersApi {
+  private final UserService userService;
+  private final UserMapper userMapper;
+
+  @Override
+  public ResponseEntity<UserResponse> createUser(CreateUserRequest createUserRequest) {
+    User mappedUser = userMapper.createUserRequestToUser(createUserRequest);
+    User createdUser = userService.createUser(mappedUser);
+    return new ResponseEntity<>(userMapper.userToUserResponse(createdUser), HttpStatus.CREATED);
+  }
+
+  @Override
+  public ResponseEntity<UserResponse> updateUserById(Long userId,
+                                                     UpdateUserRequest updateUserRequest) {
+    User mappedUser = userMapper.updateUserRequestToUser(updateUserRequest);
+    User updatedUser = userService.updateUser(userId, mappedUser);
+    return new ResponseEntity<>(userMapper.userToUserResponse(updatedUser), HttpStatus.OK);
+  }
+}
