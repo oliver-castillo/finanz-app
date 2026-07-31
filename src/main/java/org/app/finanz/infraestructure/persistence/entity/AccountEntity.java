ff49fe73735949d942d719ccd5a243e4d11c17af
Oliver Castillo <olcastillot@gmail.com>
2026-07-31 00:24:05 -0500

    feat: update Account and User entities to include relationships and change amount type

--- src/main/java/org/app/finanz/infraestructure/persistence/entity/AccountEntity.java
+++ src/main/java/org/app/finanz/infraestructure/persistence/entity/AccountEntity.java
@@ -2,11 +2,16 @@ package org.app.finanz.infraestructure.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Table(name = "\"ACCOUNTS\"")
 @Entity
@@ -25,11 +30,15 @@ public class AccountEntity extends AuditableEntity {
   private String type;
 
   @Column(name = "\"AMOUNT\"")
-  private double amount;
+  private BigDecimal amount;
 
   @Column(name = "\"CURRENCY\"")
   private String currency;
 
   @Column(name = "\"IS_EXCLUDED_FROM_STATISTICS\"")
   private boolean isExcludedFromStatistics;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "\"USER_ID\"", nullable = false)
+  private UserEntity userEntity;
 }
--- src/main/java/org/app/finanz/infraestructure/persistence/entity/UserEntity.java
+++ src/main/java/org/app/finanz/infraestructure/persistence/entity/UserEntity.java
@@ -4,13 +4,18 @@ import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Table(name = "\"USERS\"")
 @Entity
@@ -35,5 +40,13 @@ public class UserEntity extends AuditableEntity {
   private String password;
 
   @Roles
-  public String role;
+  private String role;
+
+  @OneToMany(
+      mappedBy = "userEntity",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true,
+      fetch = FetchType.LAZY
+  )
+  private List<AccountEntity> accounts;
 }
